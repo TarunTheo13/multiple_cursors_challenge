5bb7dfc17ed6412b4179d324fc0014f3087c54ac
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-01-20 18:28:27 +0000

    websocket connections receive data from other connections

--- WebSocketServer.js
+++ WebSocketServer.js
@@ -1,11 +1,16 @@
 const ws_ = require('ws');
 
 const wss = new ws_.WebSocketServer({ port: 8080 });
+const clients = [];
 
 wss.on('connection', function connection(ws, req) {
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    //console.log('received: %s', data);
+    clients.forEach(client => client.send(data.toString()));
   });
-  console.log(req.socket.remoteAddress)
+  // console.log(req.socket.remoteAddress)
+  ws.index = clients.length;
+  clients.push(ws);
   ws.send('something');
+  console.log('New client connection, no: ', ws.index)
 });
