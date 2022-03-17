9483978f1113fd54e9bb7551c792541a0da4c6e7
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-17 17:28:44 +0000

    randomised colour for individual cursors

--- Processors/CoordinateProcessor.js
+++ Processors/CoordinateProcessor.js
@@ -3,11 +3,16 @@ const coordinateProcessor = (message, self) => {
   if (!cursor) {
     cursor = document.createElement('div');
     cursor.setAttribute('class', 'cursor');
+    cursor.style.background = `rgb(${[randNum(), randNum(), randNum()]})`
     document.body.appendChild(cursor);
     return { cursor };
   }
   cursor.style.top = `${message.y}px`;
   cursor.style.left = `${message.x}px`;
+}
+
+const randNum = () => {
+  return Math.floor(Math.random() * 256)
 }
 
 export { coordinateProcessor }
