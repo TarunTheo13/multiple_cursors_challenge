6be1b930a5cf4bf7bb2839457a1e57957a2049de
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-28 18:13:58 +0100

    displaying table results through php

--- db.php
+++ db.php
@@ -6,15 +6,29 @@
     $servername = "127.0.0.1";
     $username = "root";
     $password = "Gauranga";
+    $dbname = "cursor";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    echo "Connected successfully <br><br>";
+
+    $sql = "SELECT * FROM coordinates";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " X: " . $row["x"]. " Y: " . $row["y"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
     ?>
     </pre>
   </body>
